77dccdf6c891e7931b9bacaa8ae0c1363073d5eb
Saparbek Bainazar <Saparbek28@gmail.com>
2019-06-03 17:58:38 +0600

    first commit

---
+++ Bank.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SynhroHW
+{
+    public class Bank
+    {
+        private int money = 100;
+        private object lockObject = new object();
+
+            
+        //public void Generation()
+        //{
+        //    string[] GiveTaken = { "Дали", "Забрали" };
+        //    Random ran = new Random();
+
+        //    for (int i = 0; i < money; i++)
+        //    {
+        //        switch (ran.Next(2))
+        //        {
+        //            case 0:
+        //                Console.WriteLine(GiveTaken[0]);
+        //                money += 10;
+        //                break;
+        //            case 1:
+        //                Console.WriteLine(GiveTaken[1]);
+        //                money -= 10;
+        //                break;
+        //        }
+        //    }
+        //}
+
+        public void PrintNumber()
+        {
+            //lock (lockObject)
+            //{
+                var currentThread = Thread.CurrentThread;
+                Console.WriteLine($"Поток с ID {currentThread.ManagedThreadId} начал работу");
+
+                for (int i = 0; i < 1; i++)
+                {
+                    string[] GiveTaken = { "Дали", "Забрали" };
+                    Random ran = new Random();
+
+                    for (int j = 0; j < 1; j++)
+                    {
+                        switch (ran.Next(2))
+                        {
+                            case 0:
+                                Console.WriteLine(GiveTaken[0]);
+                                money += 10;
+                                Console.WriteLine(money);
+                                break;
+                            case 1:
+                                Console.WriteLine(GiveTaken[1]);
+                                money -= 10;
+                                Console.WriteLine(money);
+                                break;
+                        }
+                    }
+                    Thread.Sleep(1000);
+                }
+
+                Console.WriteLine($"\nПоток с ID {currentThread.ManagedThreadId} завершил работу");
+            //}
+        }
+
+    }   
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SynhroHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var threads = new Thread[5];
+            var banks = new Bank();
+
+           // Timer timer = new Timer(PrintTime, null, TimeSpan.FromSeconds(0), TimeSpan.FromSeconds(1));
+
+            for(int i = 0; i < threads.Length; i++)
+            {
+                var thread = new Thread(banks.PrintNumber);
+                thread.Name = $"поток под номером {thread.ManagedThreadId}";
+                threads[i] = thread;
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Start();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
